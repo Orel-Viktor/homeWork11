b8ed6434ef4cc04bbaeaac7b20e0b20dd14c92fe
Orel-Viktor <OrelViktor93@gmail.com>
2022-10-01 14:07:45 +0300

    1parag-redy

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-import './style.css'
+// На странице есть две кнопки. При нажатии на первую кнопку пользователь должен ввести в prompt адрес ссылки, 
+// при нажатии на вторую – переадресовывается на другой сайт (по ранее введенной ссылке).
+let getLinkButton = document.querySelector('.js--getLinkButton')
+let moveLinkButton = document.querySelector ('.js--moveLinkButton')
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+getLinkButton.addEventListener('click', inputLink)
+moveLinkButton.addEventListener('click', trekLink)
+
+
+function inputLink (){
+  myLink = prompt('Введите ссылку')
+  
+}
+
+function trekLink (){
+ window.location.href = myLink
+ 
+}
