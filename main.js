5253c09c4ab21c9b4e5c37341b0c45f8808afdc3
Orel-Viktor <OrelViktor93@gmail.com>
2022-10-03 15:56:39 +0300

    use strict

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 // На странице есть две кнопки. При нажатии на первую кнопку пользователь должен ввести в prompt адрес ссылки, 
 // при нажатии на вторую – переадресовывается на другой сайт (по ранее введенной ссылке).
+'use strict'
+
 let getLinkButton = document.querySelector('.js--getLinkButton')
 let moveLinkButton = document.querySelector ('.js--moveLinkButton')
 
 getLinkButton.addEventListener('click', inputLink)
 moveLinkButton.addEventListener('click', trekLink)
 
+ let myLink
  
 function inputLink (){
- myLink = prompt('Введите ссылку')
+  myLink = prompt('Введите ссылку')
   
 }
 
