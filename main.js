9b20137ea9764e0c28b51243ec6a4a65d02a87d5
Orel-Viktor <OrelViktor93@gmail.com>
2022-10-03 14:23:44 +0300

    homework11-redy

--- main.js
+++ main.js
@@ -13,6 +13,44 @@ function inputLink (){
 }
 
 function trekLink (){
- window.location.href = myLink
- 
+location.href = myLink
+}
+
+// В папку images добавить изображения разных размеров: 1.jpg, 2.jpg, 3.jpg, 4.jpg, 5.jpg, 6.jpg, 7.jpg, 8.jpg, 9.jpg. 
+// Вывести изображение из этой папки, полученное случайным образом (Math.random). 
+// Размер изображения должен соотвецтвовать 80% размеров окна браузера (к примеру окно 1000px, то размер изображения 800px).
+
+function getRandom() {
+  return Math.random();
+}
+let randomNumber = Math.ceil((getRandom()) * 9)
+console.log(randomNumber)
+
+let swapImage = document.querySelector('.js--swapImage')
+
+swapImage.setAttribute('src' , `images/${randomNumber}.jpg`)
+
+
+// let currentWidth = (window.screen.width)
+// let currentHeight = (window.screen.height)
+// console.log(currentWidth)
+// console.log(currentHeight)
+
+console.log(window.innerWidth)
+console.log(window.innerHeight)
+
+swapImage.setAttribute ('width', window.innerWidth * 0.8)
+swapImage.setAttribute ('height', window.innerHeight * 0.8)
+
+console.log(swapImage.width)
+console.log(swapImage.height)
+
+// Меняю картинку кнопкой
+
+let swapImageButton = document.querySelector('.js--swapImageButton')
+swapImageButton.addEventListener('click' , rollImage)
+
+function rollImage(){
+  randomNumber = Math.ceil((getRandom()) * 9)
+  swapImage.setAttribute('src', `images/${randomNumber}.jpg`)
 }
